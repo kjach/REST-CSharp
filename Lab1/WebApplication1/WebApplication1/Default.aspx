9ac95ba62f4c7debac1adb6ca2d4017607f4d197
Kacper Jach <kacperini@gmail.com>
2022-03-06 20:52:46 +0100

    commit

---
+++ Lab1/WebApplication1/WebApplication1/Default.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="row">
+        <br />
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <br />
+    </div>
+
+</asp:Content>
---
+++ Lab1/Zadanie 3 - Klient/Zadanie 3 - Klient/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_3___Klient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var klient = new NaszSerwis.Service1Client();
+            var zadania = klient.zwrocZadania();
+            foreach (var zadanie in zadania)
+            {
+                label1.Text += zadanie + "\n";
+            }
+        }
+    }
+}
