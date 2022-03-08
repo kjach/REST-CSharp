5d2cc0949d4517f426e3ad935a7b1ecb592b1ae2
Kacper Jach <kacperini@gmail.com>
2022-03-08 16:23:46 +0100

    lab2 in progress

---
+++ Lab2/NetCore3WebAPIApplication/WebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductContext _context;
+        public ProductsController(ProductContext context)
+        {
+            _context = context;
+            if (_context.Products.Count() == 0)
+            {
+                _context.Products.Add(new Product { Name = "Product 1" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return _context.Products.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            var Product = _context.Products.Find(id);
+            if (Product == null)
+                return NotFound();
+            return Product;
+        }
+        [HttpPost]
+        public ActionResult<Product> PostProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+    }
+}
---
+++ Lab2/NetCore3WebAPIApplication/WebApplication1/Models/ProductContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class ProductContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+    }
+}
